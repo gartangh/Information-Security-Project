1328bfb114b588e511f85fc8686391752e2cd815
Garben Tanghe <garben.tanghe@gmail.com>
2019-04-19 19:31:38 +0100

    the beginning of something beautifull ...

--- website/server.js
+++ website/server.js
@@ -1,30 +1,29 @@
 var express = require("express");
 var app = express();
 
+app.use(express.urlencoded());
 
-app.use(express.urlencoded())
+/* serves main page */
+app.get("/", function(req, res) {
+	res.sendfile('index.html');
+});
 
- /* serves main page */
- app.get("/", function(req, res) {
-    res.sendfile('index.html');
- });
+/* serves all the static files */
+app.get(/^(.+)$/, function(req, res) { 
+	console.log('static file request : ' + req.params);
+	res.sendfile( __dirname + req.params[0]);
+});
 
- /* serves all the static files */
- app.get(/^(.+)$/, function(req, res){ 
-     console.log('static file request : ' + req.params);
-     res.sendfile( __dirname + req.params[0]); 
- });
+app.post('/submit-form', (req, res) => {
+	const nr = req.body.nr;
+	const pin = req.body.pin;
+	// ...
+	console.log(nr);
+	console.log(pin);
+	res.end();
+});
 
-
- app.post('/submit-form', (req, res) => {
-  const username = req.body.username
-  //...
-  console.log(req.body.username);
-  res.end()
-})
-
- var port = process.env.PORT || 5000;
- app.listen(port, function() {
-   console.log("Listening on " + port);
- });
-
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+	console.log("Listening on " + port);
+});
