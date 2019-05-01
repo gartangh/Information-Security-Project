4b8e8039219542d1c4c6d35f4c6fb5965be41163
brasmet <bram.desmet@ugent.be>
2019-05-01 17:27:26 +0200

    databases

--- test.js
+++ test.js
@@ -2,65 +2,41 @@ const api = require('./csvapi.js');
 
 api.initFiles();
 
-setTimeout(() => {
-  api.addParty('groen', 'Federal');
-}, 500);
-
-setTimeout(() => {
-  api.addParty('vlaams belang', 'Federal');
-}, 1000);
-
-
-setTimeout(() => {
-  api.addVote('groen', 'Federal');
-}, 1500);
-
-setTimeout(() => {
-  api.addVote('groen', 'Federal');
-}, 2000);
-
-
-setTimeout(() => {
-  api.addVote('vlaams belang', 'Federal');
-}, 2500);
-
-setTimeout(() => {
-  const auth = api.checkUserCredentials(42069, 'Password');
-  console.log(auth);
-}, 3500);
-
-
-setTimeout(() => {
-  const auth = api.checkUserCredentials(42069, 'BADWORD');
-  console.log(auth);
-}, 3500);
-
 // Users
 // (natreg, first, last, pass)
 setTimeout(() => {
   api.addUser('01406181', 'Thomas', 'Uyttenhove', 'Password');
-}, 4000);
+}, 0);
 setTimeout(() => {
   api.addUser('01500322', 'Jaime', 'Roelandts', 'Password');
-}, 4500);
+}, 500);
 setTimeout(() => {
   api.addUser('01501457', 'Garben', 'Tanghe', 'Password');
-}, 5000);
+}, 1000);
 setTimeout(() => {
   api.addUser('01502363', 'Bram', 'De Smet', 'Password');
-}, 5500);
+}, 2000);
 setTimeout(() => {
   api.addUser('01503573', 'Olivier', 'Van den Nest', 'Password');
-}, 6000);
+}, 2500);
 setTimeout(() => {
   api.addUser('01507157', 'Robbe', 'De Vilder', 'Password');
-}, 6500);
+}, 3000);
 setTimeout(() => {
   api.addUser('802000272949', 'Dirk', 'Deschrijver', 'Password');
-}, 7000);
+}, 3500);
 setTimeout(() => {
   api.addUser('801000974707', 'Eric', 'Laermans', 'Password');
-}, 7500);
+}, 4000);
 setTimeout(() => {
   api.addUser('20046160', 'Bert', 'Vankeirsbilck', 'Password');
-}, 8000);
+}, 4500);
+
+// Parties
+// (name, election)
+setTimeout(() => {
+  api.addParty('groen', 'Federal');
+}, 5000);
+setTimeout(() => {
+  api.addParty('vlaams belang', 'Federal');
+}, 5500);
